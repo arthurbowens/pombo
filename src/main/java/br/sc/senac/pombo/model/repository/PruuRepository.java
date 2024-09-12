8280e5a11201714ebc2e0fe64ceaf4f9b0ea6a5c
arthurbowens <arthurbowens18@gmail.com>
2024-09-11 21:26:11 -0300

    pruuRepository alterado

--- src/main/java/br/sc/senac/pombo/model/repository/PruuRepository.java
+++ src/main/java/br/sc/senac/pombo/model/repository/PruuRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @Repository
-public interface PruuRepository {
+public interface PruuRepository extends JpaRepository<Pruu, String> {
 
     List<Pruu> findByUserId(String userId);
 }
